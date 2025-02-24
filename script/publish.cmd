0dc33a08b1a23cd1accfc1e1cd55f46137c9c27c
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-02-24 15:55:47 +0200

    ! publish scripts

--- script/publish.cmd
+++ script/publish.cmd
@@ -1,7 +1,13 @@
 
 @echo off
 
-choice /C YN /m "ready to publish %*?"
+call secrets.cmd
+
+set PUBLISH_TOKEN=%PYPI_TOKEN%
+set UV_PUBLISH_USERNAME=__token__
+set UV_PUBLISH_PASSWORD=%PYPI_TOKEN%
+
+choice /C YN /m "Have you built the package? Ready to publish?"
 if not "%errorlevel%"=="1" (
     exit /b
 )
--- script/publish_test.cmd
+++ script/publish_test.cmd
@@ -1,3 +1,5 @@
 
 @echo off
-call %~dp0\publish.cmd --publish-url https://test.pypi.org/legacy/
+call secrets.cmd
+
+call %~dp0\publish.cmd --username=__token__ --password=%PYPI_TEST_TOKEN% --publish-url=https://test.pypi.org/legacy/
